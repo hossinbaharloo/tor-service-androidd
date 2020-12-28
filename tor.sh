be2d68b0239dcc1da44e52c38661e7130da643d7
Hossein Baharloo <54281095+hossin98@users.noreply.github.com>
2020-12-28 21:02:29 +0330

    Update tor.sh

--- tor.sh
+++ tor.sh
@@ -4,7 +4,7 @@
 echo 'Welcome'
 pkg install obfs4proxy
 echo 'UseBridges 1' >> $PREFIX/etc/tor/torrc
-echo ''
+echo '' >> $PREFIX/etc/tor/torrc
 echo 'ClientTransportPlugin obfs4 exec /data/data/com.termux/files/usr/bin/obfs4proxy'
 echo 'Bridge obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 cert=EtuUbek0ufQp3ggoGPya28Lcr6duKwOexVcqT4MopHWikUnhsyVFTG1cMzHTYVKipZDfKw iat-mode=0' >> $PREFIX/etc/tor/torrc
 echo 'Bridge obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
@@ -12,5 +12,7 @@ echo 'Bridge obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 
 echo 'Bridge obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
 echo 'Bridge obfs4 85.143.214.143:6697 2A92890043632E0C126029BED54FD68C8B224507 cert=EtuUbek0ufQp3ggoGPya28Lcr6duKwOexVcqT4MopHWikUnhsyVFTG1cMzHTYVKipZDfKw iat-mode=0' >> $PREFIX/etc/tor/torrc
 echo 'Bridge obfs4 134.209.88.233:29930 782D9BFAEF45577A24BE70C77DEA4185F51752E5 cert=Gf09UaEwYER0T1xey1xX0SUWpTS8+819ssikzCQq3TSmY+dqMfAtNyYda7ifzAV/8Fa1XA iat-mode=0' >> $PREFIX/etc/tor/torrc
+echo '' >> $PREFIX/etc/tor/torrc
+echo 'done...'
 
 
